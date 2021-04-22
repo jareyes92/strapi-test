4c1ccab932da7203fc0d9591f80e0ba5ec445ba4
Jose Antonio Reyes <jose.reyes@tacubayaconsulting.com>
2021-04-22 15:47:02 +0200

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '8e213843f3228956565c46938a55afda'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'cf041e31-5aa2-44ed-9927-31e585207156'
+};
